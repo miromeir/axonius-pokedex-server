3f26bffe1f45c9b68e3054ff2415c1979e1a807a
miromeir@post.bgu.ac.il <miromeir@post.bgu.ac.il>
2022-08-07 00:57:29 +0300

    testing github readme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Path
+from fastapi.middleware.cors import CORSMiddleware
 from jsonschema.exceptions import ValidationError
 from schema import pokemon_schema, species_schema
 from json import JSONDecodeError, loads
@@ -19,7 +20,7 @@ async def root(id: int = Path(title="Pokemon ID for pokeAPI")):
             # Validate jsonschema. See schema.py
             validate(instance=pokemon, schema=pokemon_schema)
             # Fetch flavor:
-            response = await client.get(pokemon["species"]["url"])
+            response = await client.get(pokemon["species"]["url"]) # These indices are safe due to schema validation
             species = loads(response.text)
             # Validate jsonschema. See schema.py
             validate(instance=species, schema=species_schema)
@@ -33,6 +34,7 @@ async def root(id: int = Path(title="Pokemon ID for pokeAPI")):
             raise HTTPException(status_code=400, detail="Prepare for trouble, make it double! Meow, that's right!")
         
         # Pick only the attributes we care about
+        # The following indices are safe due to our schema validation
         result = {
             "id" : pokemon["id"],
             "name" : pokemon["name"],
@@ -43,6 +45,19 @@ async def root(id: int = Path(title="Pokemon ID for pokeAPI")):
             "flavor_text": species["flavor_text_entries"][0]["flavor_text"]
         }
         return result
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
